a8f223abf6ace4cc76edc48ca759ff3ab70c4018
Eli <okara.elisha@gmail.com>
2026-02-14 07:21:40 +0100

    separate target from input features and make stratified train/test split
    stratify=y

--- rainfall_prediction_classifier.py
+++ rainfall_prediction_classifier.py
@@ -55,6 +55,9 @@ def main():
     X = df.drop(columns='RainToday', axis=1)
     y = df['RainToday']
 
+    X_train, X_test, y_train, y_test =  train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+
     print(y.value_counts())
 
 
